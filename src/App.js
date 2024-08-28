f9c0f3706370090647e089d0c489d3329ed25800
Mortuza Karzon <you@example.com>
2024-08-28 09:06:58 +0600

    restart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Error"));
+  }, []);
+
   return (
     <div className="app">
       <Header />
